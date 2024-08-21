1e72ecf639d3751f5db198bf9c9ca208ffba11fe
Jhanoo <44697627+Jhanoo@users.noreply.github.com>
2024-08-21 14:38:07 +0900

    [level 2] Title: 오픈채팅방, Time: 207.85 ms, Memory: 147 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,49 +1,54 @@
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 class Solution {
     public String[] solution(String[] record) {
-        List<List<String>> list = new ArrayList<>();
-        Map<String, String> user = new HashMap<>();
-        StringTokenizer st;
-        
-        for(String str: record) {
-            st = new StringTokenizer(str);
-            String cmd = st.nextToken();
-            String uid = st.nextToken();
-            
-            if(cmd.equals("Enter")) {
-                String name = st.nextToken();
-                user.put(uid, name);
-                
-                List<String> tmp = new ArrayList<>();
-                tmp.add(uid);
-                tmp.add("님이 들어왔습니다.");
-                
-                list.add(tmp);
-                
-            } else if(cmd.equals("Leave")) {
-                List<String> tmp = new ArrayList<>();
-                tmp.add(uid);
-                tmp.add("님이 나갔습니다.");
-                
-                list.add(tmp);
-                
-            } else if(cmd.equals("Change")) {
-                String name = st.nextToken();
-                user.put(uid, name);
-            }
-        }
-        
-        List<String> ans = new ArrayList<>();
-        
-        for(List<String> tmp : list) {
-            String name = user.get(tmp.get(0));
-            ans.add(name + tmp.get(1));
-        }
-        
-        String answer[] = ans.toArray(new String[ans.size()]);
-        
-        
-        return answer;
-    }
+		Queue<List<String>> q = new ArrayDeque<List<String>>();
+		Map<String, String> user = new HashMap<>();
+		StringTokenizer st;
+
+		for (String str : record) {
+			st = new StringTokenizer(str);
+			String cmd = st.nextToken();
+			String uid = st.nextToken();
+
+			if (cmd.equals("Enter")) {
+				String name = st.nextToken();
+				user.put(uid, name);
+
+				List<String> tmp = new ArrayList<>();
+				tmp.add(uid);
+				tmp.add("님이 들어왔습니다.");
+
+				q.offer(tmp);
+
+			} else if (cmd.equals("Leave")) {
+				List<String> tmp = new ArrayList<>();
+				tmp.add(uid);
+				tmp.add("님이 나갔습니다.");
+
+				q.offer(tmp);
+
+			} else if (cmd.equals("Change")) {
+				String name = st.nextToken();
+				user.put(uid, name);
+			}
+		}
+
+		String[] answer = new String[q.size()];
+
+		int i = 0;
+		while (!q.isEmpty()) {
+			List<String> tmp = q.poll();
+			String name = user.get(tmp.get(0));
+			answer[i++] = name + tmp.get(1);
+		}
+
+		return answer;
+	}
 }
