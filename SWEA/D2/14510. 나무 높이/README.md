af57f92d616383ee330d8736fe86229a56662aa0
Jhanoo <44697627+Jhanoo@users.noreply.github.com>
2024-09-03 11:33:52 +0900

    [D2] Title: 나무 높이, Time: 111 ms, Memory: 19,208 KB -BaekjoonHub

---
+++ SWEA/D2/14510. 나무 높이/README.md
@@ -0,0 +1,15 @@
+# [D2] 나무 높이 - 14510 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AYFofW8qpXYDFAR4) 
+
+### 성능 요약
+
+메모리: 19,208 KB, 시간: 111 ms, 코드길이: 1,429 Bytes
+
+### 제출 일자
+
+2024-09-03 11:33
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	public static void main(String[] args) throws Exception {
+
+		// System.setIn(new FileInputStream("input 14510.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		int T = Integer.parseInt(br.readLine());
+		for (int test_case = 1; test_case <= T; test_case++) {
+			sb.append("#" + test_case + " ");
+
+			int N = Integer.parseInt(br.readLine());
+			int a[] = new int[N];
+			int max = 1;
+
+			st = new StringTokenizer(br.readLine(), " ");
+			for (int i = 0; i < N; i++) {
+				a[i] = Integer.parseInt(st.nextToken());
+				max = max > a[i] ? max : a[i];
+			}
+
+			int cntOne = 0;
+			int cntTwo = 0;
+
+			for (int i = 0; i < N; i++) {
+				a[i] = max - a[i];
+				cntOne += a[i] % 2;
+				cntTwo += a[i] / 2;
+			}
+
+			if (cntOne + 2 < cntTwo) {
+				int mok = (cntTwo - cntOne) / 3;
+				int nameoge = (cntTwo - cntOne) % 3 > 1 ? 1 : 0;
+
+				cntOne += (mok + nameoge) * 2;
+				cntTwo -= mok + nameoge;
+			}
+
+			int result = 0;
+			if (cntOne > cntTwo) {
+				result = cntOne * 2 - 1;
+			} else if (cntOne == cntTwo) {
+				result = cntOne * 2;
+			} else {
+				result = cntTwo * 2;
+			}
+
+			sb.append(result);
+			sb.append("\n");
+		}
+
+		System.out.println(sb);
+	}
+
+}
