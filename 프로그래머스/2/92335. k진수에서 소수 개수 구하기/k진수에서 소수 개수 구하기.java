b9f1a88d19d55bc1a5bc8344c5b4a01f75c6491c
Jhanoo <44697627+Jhanoo@users.noreply.github.com>
2024-11-07 11:02:22 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 5.66 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -2,82 +2,52 @@ import java.util.*;
 
 class Solution {
     
-    // 1,000,000(10) = 1,212,210,202,001(3)
-	// route(1,212,210,202,001) = 1,101,004.17... < 1,101,005
-	static boolean[] isPrime;
-	static Set<String> primes;
+    public int solution(int n, int k) {
+        // n을 k진수 문자열로 변환 후 0을 기준으로 분리
+        String[] parts = changeRadix(n, k).split("0");
+        
+        int answer = 0;
+        for (String part : parts) {
+            if (part.isEmpty()) 
+                continue;
+            
+            long num = Long.parseLong(part); // 10진수로 변환
+            if (isPrime(num)) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
 
-	public int solution(int n, int k) {
-		initPrime();
+    // 10진수를 n진수로 변환하는 함수
+    static String changeRadix(int n, int k) {
+        StringBuilder sb = new StringBuilder();
+        while (n > 0) {
+            sb.append(n % k);
+            n /= k;
+        }
+        return sb.reverse().toString();
+    }
 
-		String[] nums = changeRadix(n, k).split("0");
-
-		int answer = 0;
-		for (String num : nums) {
-
-			if (num.isEmpty())
-				continue;
-
-			if (primes.contains(num)) {
-				answer++;
-			} else {
-				long tmp = Long.parseLong(num);
-
-				if (checkPrime(tmp))
-					answer++;
-			}
-		}
-
-		return answer;
-	}
-
-	static void initPrime() {
-		int n = 1101005;
-		isPrime = new boolean[n + 1];
-		Arrays.fill(isPrime, true);
-
-		primes = new HashSet<>();
-
-		// 0, 1 != 소수
-		isPrime[0] = isPrime[1] = false;
-
-		for (int i = 2; i <= n; i++) {
-
-			if (isPrime[i]) { // 소수이면
-				primes.add(i + ""); // 추가
-
-				// 소수의 배수는 전부 false 갱신
-				for (int j = i * i; j <= n && j > 0; j += i) {
-					isPrime[j] = false;
-				}
-			}
-		}
-	}
-
-	// 10진법 -> n진법 변환
-	static String changeRadix(int digit, int radix) {
-		String changed = "";
-
-		for (int i = digit; i > 0; i /= radix) {
-			changed = (i % radix) + changed;
-		}
-
-		return changed;
-	}
-
-	static boolean checkPrime(long num) {
-		// prime Set에 있는 마지막 소수는 1100977
-		if (num <= 1100977) {
-			return false;
-		}
-
-		for (String prime : primes) {
-			if (num % Long.parseLong(prime) == 0) {
-				return false;
-			}
-		}
-
-		return true;
-	}
+    // 소수 판별 함수
+    static boolean isPrime(long num) {
+        if (num <= 1) 
+            return false;
+        
+        if (num == 2 || num == 3) 
+            return true;
+        
+        if (num % 2 == 0 || num % 3 == 0) 
+            return false;
+        
+        for (long i = 5; i * i <= num; i += 6) {
+            if (num % i == 0 || num % (i + 2) == 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
 
 }
