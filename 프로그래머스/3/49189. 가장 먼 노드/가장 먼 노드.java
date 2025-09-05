606d093a08c108e0ae162085df825ba0b4d78ae6
Jhanoo <44697627+Jhanoo@users.noreply.github.com>
2025-09-05 16:51:19 +0900

    [level 3] Title: 가장 먼 노드, Time: 29.42 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,50 +1,46 @@
 import java.util.*;
 
 class Solution {
-    // dp
     public int solution(int n, int[][] edge) {
         
-        ArrayList<Integer>[] edges = new ArrayList[n + 1];
-        for (int i = 0; i <= n; i++) {
-            edges[i] = new ArrayList<>();
-        }
+        ArrayList<Integer>[] graph = new ArrayList[n + 1];
+        Arrays.setAll(graph, g -> new ArrayList<>());
         
-        // 간선 초기화
-        for (int i = 0; i < edge.length; i++) {
-            int v1 = edge[i][0];
-            int v2 = edge[i][1];
+        // 인접 노드 연결
+        for (int[] e : edge) {
+            int a = e[0];
+            int b = e[1];
             
-            edges[v1].add(v2);
-            edges[v2].add(v1);
+            graph[a].add(b);
+            graph[b].add(a);
         }
         
-        int[] vertices = new int[n + 1];
-        Arrays.fill(vertices, -1); // 거리를 -1로 초기화
+        int[] dist = new int[n + 1]; // 거리 배열
+        Arrays.fill(dist, -1); // -1: 미방문 으로 초기화
         
         // BFS
         Queue<Integer> q = new ArrayDeque<>();
         q.offer(1); // 1번 노드 삽입
-        vertices[1] = 0; // 1번 노드 까지의 거리
+        dist[1] = 0; // 1번 노드 까지의 거리
         
         int maxDist = 0; // 가장 멀리 떨어진 노드까지의 거리
         
         while(!q.isEmpty()) {
-            int v = q.poll(); // v번 노드
-            int dist = vertices[v]; // 1번 노드 ~ v번 노드까지의 최단 거리
+            int v = q.poll();
+            int d = dist[v];
+            if (d > maxDist) maxDist = d;
             
-            maxDist = Math.max(maxDist, dist); // 거리가 멀면 갱신
-            
-            for (int next : edges[v]) {
-                if (vertices[next] != -1) continue; // 최단거리로 방문한 적이 있는 노드는 스킵
+            for (int next : graph[v]) {
+                if (dist[next] != -1) continue; // 방문한 노드는 스킵
                 
+                dist[next] = d + 1;
                 q.offer(next);
-                vertices[next] = dist + 1;
             }
         }
         
         int answer = 0;
         for (int i = 1; i <= n; i++) {
-            if (vertices[i] == maxDist)
+            if (dist[i] == maxDist)
                 answer++;
         }
         
