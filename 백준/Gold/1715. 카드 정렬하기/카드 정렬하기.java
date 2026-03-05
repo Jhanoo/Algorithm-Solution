cbc2016c4254863309b47d72f418c6e562fa2de7
Jhanoo <44697627+Jhanoo@users.noreply.github.com>
2026-03-06 00:26:40 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 324 ms, Memory: 24908 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -1,38 +1,28 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.io.*;
+import java.util.*;
 
+public class Main {
 
-class Main {
-    
     public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-		int N = Integer.parseInt(br.readLine());
-		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(N);
 
-		for (int i = 0; i < N; i++) {
-			pq.offer(Integer.parseInt(br.readLine()));
-		}
+        for (int i = 0; i < N; i++) {
+            pq.offer(Integer.parseInt(br.readLine()));
+        }
 
-		if (pq.size() == 1) {
-			System.out.println(0);
-			return;
-		}
+        int ans = 0;
+        while (pq.size() > 1) {
+            int a = pq.poll();
+            int b = pq.poll();
 
-		int result = 0;
-		while (!pq.isEmpty()) {
-			int a = pq.poll();
-			if (pq.isEmpty()) {
-				break;
-			}
-			int b = pq.poll();
-
-			result += a + b;
-			pq.offer(a + b);
-		}
-
-		System.out.println(result);
-	}
-    
+            ans += a + b;
+            pq.offer(a + b);
+        }
+        
+        System.out.println(ans);
+    }
 }
